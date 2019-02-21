398535ce7cd2a5634170e2c10bbb2cfaf42073ae
Alex van Stekelenborg <35575349+AlStek@users.noreply.github.com>
2019-02-21 18:49:36 -0500

    added buttons to control bedroom lamp
    figured out json and ajax and basic hue api to toggle my bedroom light on and off

---
+++ javascript/hue.js
@@ -0,0 +1,27 @@
+$('#get_lights').click(function(){
+    $.getJSON('https://10.0.0.78/api/rVHJqasih6l0fzSXE4m8KfH9ljAMq6bXtZmRdcTf/lights', function (data) {
+        console.log(data);
+        data = JSON.stringify(data);
+        $('#json').append(data);
+
+
+
+        });
+
+	})
+
+$('#light_on').click(function(){$.ajax({
+        url: 'http://10.0.0.78/api/rVHJqasih6l0fzSXE4m8KfH9ljAMq6bXtZmRdcTf/lights/4/state',
+        type: 'PUT',
+        data: JSON.stringify({"on": true}),
+        success: function(){alert("Light 4 has been turned on")}}
+    )}
+)
+
+$('#light_off').click(function(){$.ajax({
+        url: 'http://10.0.0.78/api/rVHJqasih6l0fzSXE4m8KfH9ljAMq6bXtZmRdcTf/lights/4/state',
+        type: 'PUT',
+        data: JSON.stringify({"on":false}),
+        success: function(){alert("Light 4 has been turned off")}}
+    )}
+)
