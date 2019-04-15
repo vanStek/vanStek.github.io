fae17fefcca01a8780fd4c44970165753f419804
Alex van Stekelenborg <35575349+AlStek@users.noreply.github.com>
2019-04-15 19:13:19 -0400

    css touch ups

--- javascript/weather.js
+++ javascript/weather.js
@@ -1,23 +1,45 @@
 $(document).ready(function(){
     //assigns request function to request button/retrieves lat long
     $("#request").click(request);
+
+    $(document).on('keypress',function(e) {
+    if(e.which == 13) {
+        request();
+    }
+});
+
+
 });
 
 //accepts lat and long values from above, sends get request
 function request(){
 
 
-    $.getJSON('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/8e46d8383f69f9f889a13b1a00838106/'+ $('#lat').val() +',' + $('#long').val(),  function (data){
-        //string from JSON to add to result div
-        output = "<br /> Location: "  + data.timezone
-                +"<br />Summary: " + data.hourly.summary
-                + "<br />Temperature: " + JSON.stringify(data.currently.temperature)
-                + "<br /> Precipitation Probability: " + JSON.stringify(data.currently.precipProbability)
-                + "<br />Humidity: " + data.currently.humidity * 100 +"%";
+    $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address='+$('#address').val()+'&key=AIzaSyBM53VkGTsTDMYZrbQImxOJd4ESAJnqXfU', function(data){
+        lat =  data.results[0].geometry.location.lat;
+        long = data.results[0].geometry.location.lng;
 
-        //adds weather to result div
-        $("#result").html(output);
+        console.log(lat);
+        console.log(long);
+
+        $.getJSON('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/8e46d8383f69f9f889a13b1a00838106/'+ lat +',' + long,  function (data){
+            //string from JSON to add to result div
+            output = data.timezone
+                    + "<br />" + data.hourly.summary
+                    + "<br />" + JSON.stringify(data.currently.temperature)
+                    + "<br />" + data.currently.precipProbability * 100 +"%"
+                    + "<br />" + data.currently.humidity * 100 +"%";
+
+            //adds weather to result div
+            $("#hide").css('visibility', 'visible');
+            $("#result").html(output);
+
+        });
+
+
 
 
     });
+
+
 }
